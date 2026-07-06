3e0a1a23095690bef8d6331d09de0ff8d084e7dd
Jumpei SASAI <jun794uh1603ti@gmail.com>
2026-07-06 10:45:49 +0900

    test code kadai

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,22 @@ public class CalculateTest {
         int expected = 5;
         assertEquals(expected, calculator.sum(2, 3));
     }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+        printResult(12,10);
+        printResult(13,9);  
+    }
+
+    public static void printResult(int age, int size) {
+        Calculate calc = new Calculate();
+        int individual = calc.getIndividualFee(age);
+        int total = calc.getTotalFee(age, size);
+
+        if (size >= 10) {
+            System.out.println("Age" + age + ",Size" + size + ":Individual" + individual + ",Total" + total + "(Discount applied)");
+        } else {
+            System.out.println("Age" + age + ",Size" + size + ":Individual" + individual + ",Total" + total);
+        }
+    }
 }
